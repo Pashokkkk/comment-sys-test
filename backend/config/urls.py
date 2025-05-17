1f760e29b2ea3bd716866fbefb92d680dab5eae8
Pavlo Khomliuk <103265356+Pashokkkk@users.noreply.github.com>
2025-05-17 17:40:50 +0100

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -31,3 +31,5 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
