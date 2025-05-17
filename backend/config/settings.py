f709cb064634af53331fb03ee00990365df82cd6
Pavlo Khomliuk <103265356+Pashokkkk@users.noreply.github.com>
2025-05-17 17:16:43 +0100

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-9wh-1*er%f*hce&bm*=pr^eyf#(ed(g3s34$esokb!8bpiq149
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['yourdomain.com', 'localhost']
-
+ALLOWED_HOSTS = ['comment-sys-test.onrender.com', 'localhost']
 
 
 # Application definition
