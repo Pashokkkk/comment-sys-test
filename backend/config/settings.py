caabdfcb58da8afdc2822df9d00be3da2db2d0ca
Pavlo Khomliuk <103265356+Pashokkkk@users.noreply.github.com>
2025-05-22 22:52:24 +0100

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'comments_db',     
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'db',
-        'PORT': '3306',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
     }
 }
 
