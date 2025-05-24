d5f4664136caea49852c36c4e24362e3b09feda1
Pavlo Khomliuk <103265356+Pashokkkk@users.noreply.github.com>
2025-05-24 15:18:20 +0100

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,15 +3,13 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.decorators.cache import never_cache
-import logging
+from django.views.static import serve
 from comments.views import UserCommentListCreateAPIView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
-# Define API and admin routes
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('comments.urls')),
@@ -23,6 +21,13 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
 
 urlpatterns += [
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
