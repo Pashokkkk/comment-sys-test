36fdc34cf31571dd7405c83458a8e8f0bd1bc392
Pavlo Khomliuk <103265356+Pashokkkk@users.noreply.github.com>
2025-05-25 17:35:40 +0100

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -149,7 +149,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 25
+    'PAGE_SIZE': 25,
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 }
 
 # set up Redis and Celery
