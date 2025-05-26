83aaf72b159013a7e7ec1088d179397d4dc796b4
Pavlo Khomliuk <103265356+Pashokkkk@users.noreply.github.com>
2025-05-26 15:33:13 +0100

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -157,7 +157,8 @@ REST_FRAMEWORK = {
 
 # set up Redis and Celery
 
-CELERY_BROKER_URL = 'redis://redis:6379/0'
+# CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379")
 CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 
 # Caching
