4b1e5bc429ae1da9ebb5a48ecad40f8627f48d53
Pavlo Khomliuk <103265356+Pashokkkk@users.noreply.github.com>
2025-05-28 10:50:45 +0100

    Update urls.py

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import captcha_refresh, UserCommentListCreateAPIView
 
 urlpatterns = [
+    path('', welcome_api),
     path('comments/', UserCommentListCreateAPIView.as_view(), name='comment-list-create'),
     path('captcha/refresh/', captcha_refresh),
 ]
